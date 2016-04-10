5eec9ec5a42782e1026e54434efcf396a40d3b29
tripleCC <triplec.linux@gmail.com>
2016-04-10 19:00:14 +0800

    Initial Commit

---
+++ TPCQuickAttribute/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TPCQuickAttribute
+//
+//  Created by tripleCC on 16/4/10.
+//  Copyright © 2016年 tripleCC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
